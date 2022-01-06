fb5f848cb3d65cc802a68b1f55c3bae970081dab
Bart Cerneels <bart@theapplitree.be>
2022-01-06 09:25:17 +0100

    code doc++

--- time-blaster/time-blaster.py
+++ time-blaster/time-blaster.py
@@ -14,6 +14,8 @@ frames = collections.deque((), 40)
 buffer = array.array('L')
 micros = 0
 
+### This irq creates frames containing pulse lengths.
+# It's designed to be similar to the ouput of the ESP32 RMT
 def handle_blaster_irq(pin):
     global buffer
     global micros
@@ -21,19 +23,21 @@ def handle_blaster_irq(pin):
     delta = time.ticks_diff(time.ticks_us(), micros)
     micros = time.ticks_us()
     if delta > start_pulse*pulse_us:
+        # Start detected
         buffer = []
         return
 
     buffer.append(delta)
 
     if (len(buffer) == (framelength + 1)*2):
+        # full frame received
         frames.append(buffer)
         buffer = []
 
 
 blaster_link.irq(handle_blaster_irq, Pin.IRQ_FALLING | Pin.IRQ_RISING)
 
-def print_blaster_data(data):
+def print_blaster_frame(data):
     for delta in data:
         pulse = (delta/pulse_us);
         if pulse > (start_pulse-1):
@@ -50,7 +54,7 @@ def print_blaster_data(data):
 while True:
     try:
         data = frames.popleft()
-        print_blaster_data(data)
+        print_blaster_frame(data)
     except IndexError:
         time.sleep(1)
     except KeyboardInterrupt:
