700706fe039d93a722e4a8d5721440c229119a23
Bart Cerneels <bart@theapplitree.be>
2022-01-07 09:44:33 +0100

    Attempt to decode using uctypes

--- time-blaster/time-blaster.py
+++ time-blaster/time-blaster.py
@@ -1,12 +1,46 @@
+from ctypes import addressof
 from machine import Pin
 import array
 import collections
 import sys
 import time
+import uctypes
 
 pulse_us = 4000
 start_pulse = 16
 framelength = 8
+
+class Data():
+
+    protocol_descr = {
+        "color":    uctypes.BFUINT8 | 0 | 0 << uctypes.BF_POS | 2 << uctypes.BF_LEN,
+        "fired":    uctypes.BFUINT8 | 0 | 2 << uctypes.BF_POS | 1 << uctypes.BF_LEN,
+        "unused":   uctypes.BFUINT8 | 0 | 3 << uctypes.BF_POS | 4 << uctypes.BF_LEN,
+        "parity":   uctypes.BFUINT8 | 0 | 7 << uctypes.BF_POS | 1 << uctypes.BF_LEN,
+    }
+
+    def __init__(self) -> None:
+        self.bits = bytearray(1)
+
+    def __getitem__(self, n):
+        return (self.bits[0] >> n) & 1
+
+    def __setitem__(self, n, v):
+        if v:
+            self.bits[0] |= v << n
+        else:
+            self.bits[0] &= ~(1 << n)
+
+    def str_decoded(self):
+        data = uctypes.struct(addressof(self.bits), Data.protocol_descr)
+        output = 'Data:\n' \
+                 '\t color: {}\n' \
+                 '\t fired: {}\n' \
+                 '\tparity: {}\n'.format(data.color, data.fired, data.parity)
+        return output
+
+    def __str__(self):
+        return '{:#010b}'.format(self.bits[0])
 
 class Receive:
     frames = collections.deque((), 40)
@@ -53,7 +87,7 @@ class Receive:
         if (len(frame) != (framelength + 1)*2):
             return
 
-        data = array.array('b')
+        data = Data()
         for i in range(len(frame)/2):
             delta0 = frame[i*2]
             delta1 = frame[i*2+1]
@@ -63,9 +97,9 @@ class Receive:
                 continue
 
             if (delta0*2 < delta1):
-                data.append(1)
+                data[8-i] = 1
             else:
-                data.append(0)
+                data[8-i] = 0
 
         return data
 
@@ -78,6 +112,7 @@ while True:
         blaster_link.print_blaster_frame(frame)
         data = blaster_link.decode_blaster_frame(frame)
         print(data)
+        print(data.str_decoded())
     except IndexError:
         time.sleep(1)
     except KeyboardInterrupt:
