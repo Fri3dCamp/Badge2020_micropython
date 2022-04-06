766387abef2eaf00698ef377b6f6f3e29157c109
Bart Cerneels <bart@theapplitree.be>
2022-04-06 20:56:55 +0200

    Enable display backlight via accelerometer interrupt

--- manifest.py
+++ manifest.py
@@ -1,3 +1,4 @@
+freeze("micropython-lis2hh12", "lis2hh12.py")
 include("manifest_micro-gui.py")
 freeze("packages")
 include("$(PORT_DIR)/boards/manifest.py")
--- packages/boot.py
+++ packages/boot.py
@@ -2,6 +2,14 @@
 #import esp
 #esp.osdebug(None)
 
+from machine import SoftI2C, Pin
+from lis2hh12 import LIS2HH12, SF_G
+
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
+sensor = LIS2HH12(i2c, address=0x18, sf=SF_G)
+# enable the ACC interrupt to turn on backlight
+sensor.enable_act_int()
+
 import settings
 
 if settings.get('BLE-beacon_enabled'):
