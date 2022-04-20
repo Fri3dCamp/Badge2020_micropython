37273bb05be2df88f4698d29802b0d27a85bf235
Bart Cerneels <bart@theapplitree.be>
2022-04-20 22:26:35 +0200

    LED feedback on touch

--- packages/boot.py
+++ packages/boot.py
@@ -4,11 +4,21 @@
 
 from machine import SoftI2C, Pin
 from lis2hh12 import LIS2HH12, SF_G
+from neopixel import NeoPixel
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
 imu = LIS2HH12(i2c, address=0x18, sf=SF_G)
 # enable the ACC interrupt to turn on backlight
 imu.enable_act_int()
+
+neopixel_pin = Pin(2, Pin.OUT)
+neopixels = NeoPixel(neopixel_pin, 5)
+neopixels[0] = (0,0,0)
+neopixels[1] = (0,0,0)
+neopixels[2] = (0,0,0)
+neopixels[3] = (0,0,0)
+neopixels[4] = (0,0,0)
+neopixels.write()
 
 import settings
 
