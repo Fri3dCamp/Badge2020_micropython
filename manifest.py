4ebbd00d9de9932d7bddbf7a064709cff83198c5
Bart Cerneels <bart@theapplitree.be>
2022-06-08 18:11:44 +0200

    Include micropython-lib for compat with badge.team

--- manifest.py
+++ manifest.py
@@ -1,4 +1,4 @@
 freeze("micropython-lis2hh12", "lis2hh12.py")
 include("manifest_micro-gui.py")
 freeze("packages")
-include("$(PORT_DIR)/boards/manifest.py")
+include("$(PORT_DIR)/boards/manifest_release.py")
