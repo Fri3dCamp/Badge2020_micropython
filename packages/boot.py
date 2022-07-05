65a950be17c03747c1185bcfe9f1773f74d51ce9
Bart Cerneels <bart@theapplitree.be>
2022-07-05 16:47:11 +0200

    Holding boot 5 seconds recovers menu

--- packages/boot.py
+++ packages/boot.py
@@ -3,9 +3,11 @@
 #esp.osdebug(None)
 
 from machine import SoftI2C, Pin
+import machine
 from lis2hh12 import LIS2HH12, SF_G
 from neopixel import NeoPixel
 import buttons
+import uasyncio
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
 imu = LIS2HH12(i2c, address=0x18, sf=SF_G)
@@ -23,6 +25,22 @@ neopixels.write()
 
 import settings
 
+def recover_menu():
+    print("Recovering menu!")
+    try:
+        settings.remove('apps.autorun')
+    except KeyError:
+        # can happen when no apps.autorun is set
+        pass
+    settings.store()
+    machine.reset()
+
+def execute_repl():
+    print("Executing REPL!")
+    settings.set('apps.autorun', 'REPL')
+    settings.store()
+    machine.reset()
+
 if settings.get('BLE-beacon_enabled'):
     import BLE_beacon
 
@@ -30,15 +48,37 @@ app = settings.get('apps.autorun')
 if app == None:
     app = "frozen_apps.menu"
 
-if buttons.boot_button.value():
-    app = "apps.menu"
 
-if app and not app == "shell":
+async def check_recover_button(pin):
+    countdown = 5
+    while pin.value() == 0:
+        print('Hold for {} seconds to recover main menu.'.format(countdown))
+        countdown -= 1
+        if (countdown == 0):
+            recover_menu()
+        await uasyncio.sleep_ms(1000)
+
+
+# Callback for the boot pin IRQ
+def hold_to_recover(pin):
+    if pin.value() == 0:
+        print("Boot button pressed.")
+        # start check task
+        uasyncio.run(check_recover_button(pin))
+        
+
+buttons.boot_pin.irq(hold_to_recover)
+
+if app and not app == "REPL":
     try:
-		print("Starting app '%s'..." % app)
+		print("Starting app '{}'...".format(app))
 		if app:
 			__import__(app)
     except BaseException as e:
         print("Exception happened in app:",  e)
         settings.remove('apps.autorun')
         settings.store()
+        
+else:
+    print("REPL is running.")
+    print("Press Boot button after power-on to run main menu.")
